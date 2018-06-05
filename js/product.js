d69581d646cdbf1893bcca3449d33e052bb6d18c
Luoluolinzhi <38150666+Luoluolinzhi@users.noreply.github.com>
2018-06-05 16:39:00 +0800

    豆瓣市集

--- js/product.js
+++ js/product.js
@@ -1,94 +1,4 @@
 $(function(){
-// var listdata = {
-	// 	result : {
-	// 		"status" : true ,
-	// 		"data" : {
-	// 			"list" : [
-	// 				{
-	// 					"id" : "1" ,
-	// 					"image" : "e020e63fbde3646.jpg",
-	// 					"title" : "豆瓣收藏夹" ,
-	// 					"introduce" : "万千票据一本搞定，珍藏你的回忆" ,
-	// 					"price" : "￥99" ,
-	// 					"type" : "热卖" ,
-	// 				},
-	// 				{
-	// 					"id" : "2" ,
-	// 					"image" : "1c8bdf9ec12dee4.jpg" ,
-	// 					"title" : "豆瓣心茶" ,
-	// 					"introduce" : "联手百年茶庄王德传，精心窖制花茶" ,
-	// 					"price" : "￥68" ,
-	// 					"type" : "热卖" ,
-	// 				},
-	// 				{
-	// 					"id" : "3" ,
-	// 					"image" : "cf603f96874d866.jpg" ,
-	// 					"title" : "双肩包·「三刻」" ,
-	// 					"introduce" : "双肩斜挎瞬间变形，多仓储存容量大" ,
-	// 					"price" : "￥499" ,
-	// 					"type" : "新品" ,
-	// 				},
-	// 				{
-	// 					"id" : "4" ,
-	// 					"image" : "94610e62b8d7936.jpg" ,
-	// 					"title" : "精品挂耳咖啡" ,
-	// 					"introduce" : "甄选COE获奖精品咖啡，香浓可口" ,
-	// 					"price" : "￥56" ,
-	// 					"type" : "新品" ,
-	// 				},
-	// 				{
-	// 					"id" : "5" ,
-	// 					"image" : "b5dc77cf3bd7a20.jpg" ,
-	// 					"title" : "手机壳·电影畅想系列" ,
-	// 					"introduce" : "经典电影元素图案，防磨防刮抗摔打" ,
-	// 					"price" : "￥48" ,
-	// 					"type" : "新品" ,
-	// 				},
-	// 				{
-	// 					"id" : "6" ,
-	// 					"image" : "aa69db404b73b99.jpg" ,
-	// 					"title" : "布面精装本" ,
-	// 					"introduce" : "落笔顺畅不洇墨，在书写中相遇世界" ,
-	// 					"price" : "￥48" ,
-	// 					"type" : "热卖" ,
-	// 				},
-	// 				{
-	// 					"id" : "7" ,
-	// 					"image" : "4d784545429b759.jpg" ,
-	// 					"title" : "帆布包·早餐系列" ,
-	// 					"introduce" : "简约版型，多层收纳，清爽文艺百搭" ,
-	// 					"price" : "￥149" ,
-	// 					"type" : "热卖" ,
-	// 				},
-	// 				{
-	// 					"id" : "8" ,
-	// 					"image" : "025756b97d6de64.jpg" ,
-	// 					"title" : "豆瓣电影日历" ,
-	// 					"introduce" : "高分片单，翻页设计，陪你感受时光" ,
-	// 					"price" : "￥98" ,
-	// 					"type" : "售罄" ,
-	// 				}
-				
-	// 			]
-	// 		}
-	// 	}
-// }
-// $.ajax({
-	// 	url : "",
-	// 	type : "get",
-	// 	dataType : "json",
-	// 	success : function(res){
-			
-	// 	},
-	// 	error : function(res){
-	// 		var blogStr = "";
-	// 		var t = listdata.result.data.list;
-	// 		for(var i=0;i<t.length;i++){
-	// 			blogStr += "<div class='new-product-list-a'><div class='new-product-list-a-img'><img src='./image/"+t[i].image+"'></div><div class='new-product-list-a-content'> <h4>"+t[i].title+"</h4><p>"+t[i].introduce+"</p><strong>"+t[i].price+"</strong><a>"+t[i].type+"</a></div></div>"			
-	// 		}
-	// 		$(".new-product-list").html(blogStr);
-	// 	}
-// })
 	 window.onload = function() {
         var swiper = new Swiper('.swiper-container', {   
             //循环            
@@ -230,55 +140,55 @@ $(function(){
 	            {
 	                "id":"1",
 	                "title":"123",
-	                "image":"https://img3.doubanio.com/view/dianpu_page_widget/raw/public/e020e63fbde3646.jpg",
+	                "image":"https://img3.doubanio.com/img/files/file-1527764147-0.jpg",
 	                "url":"2",
-	                "createtime":"2018-06-05 15:35:58",
-	                "updatetime":"2018-06-05 15:35:58",
+	                "createtime":"2018-06-05 15:42:36",
+	                "updatetime":"2018-06-05 15:42:36",
 	                "status":"1"
 	            },
 	            {
 	                "id":"2",
 	                "title":"456",
-	                "image":"https://img3.doubanio.com/view/dianpu_page_widget/raw/public/1c8bdf9ec12dee4.jpg",
+	                "image":"https://img3.doubanio.com/img/files/file-1525686721-1.jpg",
 	                "url":"3",
-	                "createtime":"2018-06-05 15:36:18",
-	                "updatetime":"2018-06-05 15:36:18",
+	                "createtime":"2018-06-05 15:42:42",
+	                "updatetime":"2018-06-05 15:42:42",
 	                "status":"1"
 	            },
 	            {
 	                "id":"3",
 	                "title":"789",
-	                "image":"https://img3.doubanio.com/view/dianpu_page_widget/raw/public/cf603f96874d866.jpg",
+	                "image":"https://img3.doubanio.com/img/files/file-1524881486-0.jpg",
 	                "url":"4",
-	                "createtime":"2018-06-05 15:36:28",
-	                "updatetime":"2018-06-05 15:36:28",
+	                "createtime":"2018-06-05 15:42:47",
+	                "updatetime":"2018-06-05 15:42:47",
 	                "status":"1"
 	            },
 	            {
 	                "id":"4",
 	                "title":"好看",
-	                "image":"https://img3.doubanio.com/view/dianpu_page_widget/raw/public/94610e62b8d7936.jpg",
+	                "image":"https://img3.doubanio.com/img/files/file-1524122635-0.jpg",
 	                "url":"www.baidu.com",
-	                "createtime":"2018-06-05 15:36:36",
-	                "updatetime":"2018-06-05 15:36:36",
+	                "createtime":"2018-06-05 15:42:53",
+	                "updatetime":"2018-06-05 15:42:53",
 	                "status":"1"
 	            },
 	            {
 	                "id":"5",
 	                "title":"好玩222",
-	                "image":"https://img3.doubanio.com/view/dianpu_page_widget/raw/public/b5dc77cf3bd7a20.jpg",
+	                "image":"https://img3.doubanio.com/img/files/file-1524199263-0.jpg",
 	                "url":"http://www.sina.com",
-	                "createtime":"2018-06-05 15:37:25",
-	                "updatetime":"2018-06-05 15:37:25",
+	                "createtime":"2018-06-05 15:43:00",
+	                "updatetime":"2018-06-05 15:43:00",
 	                "status":"1"
 	            },
 	            {
 	                "id":"6",
 	                "title":"xixi",
-	                "image":"/Public/Home/images/16.jpg",
+	                "image":"https://img3.doubanio.com/img/files/file-1527764147-0.jpg",
 	                "url":"5",
-	                "createtime":"2018-06-05 15:19:10",
-	                "updatetime":"2018-06-05 15:19:10",
+	                "createtime":"2018-06-05 15:43:01",
+	                "updatetime":"2018-06-05 15:43:01",
 	                "status":"1"
 	            }
 	        ]
@@ -289,19 +199,36 @@ $(function(){
 		dataType:'json',
 		type:'get',
 		success: function(res){
-			if(error_no == 0){
+			if(res.error_no == 0){
+				//头部轮播图
+				var g = res.data.banner;
+				var imagesLists = "";
+				for (var j = 0; j < g.length; j++){
+					imagesLists += "<div class='swiper-slide'><img src='"+g[j].image+"'></div>";
+				}
+				$(".swiper-wrapper").html(imagesLists);
+				//商品列表
 				var t = res.data.goods;
 				var goodsLists = "";
-				for(var i=0; i<t.length; i++){
-					goodsLists += "<div class='new-product-list-a'><div class='new-product-list-a-img'><img src='"+t[i].image+"'></div><div class='new-product-list-a-content'><h4>"+t[i].goods_name+"</h4><p>"+t[i].goods_info+"</p><strong>"+t[i].price+"</strong><a>"+t[i].tag_id+"</a></div></div>";
+				for(var i = 0; i < t.length; i++){
+					goodsLists += "<div class='new-product-list-a'><div class='new-product-list-a-img'><img src='"+t[i].image+"'></div><div class='new-product-list-a-content'><h4>"+t[i].goods_name+"</h4><p>"+t[i].goods_info+"</p><strong>￥"+t[i].price/100+"</strong><a>"+t[i].tag_id+"</a></div></div>";
 				}
 				$(".new-product-list").html(goodsLists);
 			}else{
-				alert(msg);
+				alert(res.msg);
 			}
 		},
-		error: function(res){
-			alert("网络错误!");
+		error: function(){
+			// alert("网络错误!");
+			//商品列表
+				var t = res.data.goods;
+				var goodsLists = "";
+				for(var i = 0; i < t.length; i++){
+					goodsLists += "<div class='new-product-list-a'><div class='new-product-list-a-img'><img src='"+t[i].image+"'></div><div class='new-product-list-a-content'><h4>"+t[i].goods_name+"</h4><p>"+t[i].goods_info+"</p><strong>￥"+t[i].price/100+"</strong><a>"+t[i].tag_id+"</a></div></div>";
+				}
+				$(".new-product-list").html(goodsLists);
 		}
 	})
+
 });
+
