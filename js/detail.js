5a719e62aa51497027a0b3ae53b9ed4949df4afa
e <1125325008@qq.com>
2018-06-07 21:01:56 +0800

    豆瓣市集

--- js/detail.js
+++ js/detail.js
@@ -24,6 +24,20 @@ $(function(){
     	$(".ceng").hide();
     	$("body").removeClass("over")
     })
+    //假数据渲染
+    var res ={
+        "error_no":0,
+        "msg":"",
+        "data":{
+            "info":{
+                "id":"1",
+                "title":"豆瓣收藏夹",
+                "price":"9900",
+                "desc":"万千票据一本搞定，珍藏你的回忆",
+                "image":"https://img3.doubanio.com/view/dianpu_page_widget/raw/public/e020e63fbde3646.jpg"
+            }
+        }
+    }
     //从localStorage里获得对应商品的id
     var id_no = localStorage.getItem("id")
     $.ajax({
@@ -42,8 +56,8 @@ $(function(){
     			$(".topImage").html(imageList);
                 //点击"立即购买"对应的商品信息
                 var goodsInfo = "";
-                goodsInfo += "<div class='p-list clearfix'><img src='"+g.image+"'><div class='p-intrp'><div class='p-intrp-a'>"+g.title+"</div><div class='p-intrp-b'>￥"+g.price/100+"</div><div class='p-intrp-c'>满 ￥88 包邮 </div><div class='p-intrp-d'>已选 “森林绿” </div></div></div><div class='p-choice'><p>款式:</p><div>森林绿</div></div><div class='p-num'><p>数量:</p><div class='p-jiajian clearfix'><div class='jian'>-</div><div class='num'>1</div><div class='jia'>+</div></div></div>";
-                $(".list").append(goodsInfo);
+                goodsInfo += "<div class='p-list clearfix'><img src='"+g.image+"'><div class='p-intrp'><div class='p-intrp-a'>"+g.title+"</div><div class='p-intrp-b'>￥"+g.price/100+"</div><div class='p-intrp-c'>满 ￥88 包邮 </div><div class='p-intrp-d'>已选 “森林绿” </div></div></div><div class='p-choice'><p>款式:</p><div>森林绿</div></div></div>";
+                $(".goodsInfo").append(goodsInfo);
     		}else{
     			alert(res.msg)
     		}
@@ -57,8 +71,17 @@ $(function(){
             $(".topImage").html(imageList);
             //点击"立即购买"对应的商品信息
             var goodsInfo = "";
-            goodsInfo += "<div class='p-list clearfix'><img src='"+g.image+"'><div class='p-intrp'><div class='p-intrp-a'>"+g.title+"</div><div class='p-intrp-b'>￥"+g.price/100+"</div><div class='p-intrp-c'>满 ￥88 包邮 </div><div class='p-intrp-d'>已选 “森林绿” </div></div></div><div class='p-choice'><p>款式:</p><div>森林绿</div></div><div class='p-num'><p>数量:</p><div class='p-jiajian clearfix'><div class='jian'>-</div><div class='num'>1</div><div class='jia'>+</div></div></div>";
-            $(".list").append(goodsInfo);
+            goodsInfo += "<div class='p-list clearfix'><img src='"+g.image+"'><div class='p-intrp'><div class='p-intrp-a'>"+g.title+"</div><div class='p-intrp-b'>￥"+g.price/100+"</div><div class='p-intrp-c'>满 ￥88 包邮 </div><div class='p-intrp-d'>已选 “森林绿” </div></div></div><div class='p-choice'><p>款式:</p><div>森林绿</div></div></div>";
+            $(".goodsInfo").append(goodsInfo);
     	}
+    })
+     //详情弹出层
+    //点击立即购买
+    $(".p-buy-a").click(function(){
+        location.href = "./ordertmp.html"
+    })
+    //点击加入购物车
+    $(".p-buy-b").click(function(){
+        
     })
 })
