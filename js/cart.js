09ee271bb34168d74fd842790ba9af7c2bbc9079
e <1125325008@qq.com>
2018-06-10 17:17:20 +0800

    豆瓣市集

--- js/cart.js
+++ js/cart.js
@@ -37,11 +37,131 @@ $(function(){
 				var goodsStr = "";
 				var g = res.data.cart;
 				for(var i=0; i<g.length; i++){
-					goodsStr += "<div class='goodsList'><span class='checkIcon'></span><div class='goodsInfo clearfix'><img src='"+g[i].image+"'><div class='info'><h6>"+g[i].goods_name+"</h6><p>玫瑰乌龙+玫瑰红茶</p><div class='numChange'><span class='numJian'>-</span><span class='num'>"+g[i].goods_num+"</span><span class='numJia'>+</span></div></div><div class='del'><h6>删除</h6><span>￥"+g[i].goods_price/100+"</span></div></div></div>"
+					goodsStr += "<div class='goodsList' data-id='"+g[i].id+"'><span class='checkIcon checkIconSpan'></span><div class='goodsInfo clearfix'><img src='"+g[i].image+"'><div class='info'><h6>"+g[i].goods_name+"</h6><p>玫瑰乌龙+玫瑰红茶</p><div class='numChange'><span class='numJian'>-</span><span class='num'>"+g[i].goods_num+"</span><span class='numJia'>+</span></div></div><div class='del'><h6 class='deleteGoods'>删除</h6><span>￥"+g[i].goods_price/100+".00</span></div></div></div>";
 				}
 				$(".goodsLists").html(goodsStr);
+				//数量的增减
+			 	$(".numJian").click(function(){
+			        var num = parseInt($(this).next(".num").text());
+			        var idNo = $(this).closest(".goodsList").data("id");
+			        if(num == 1){
+			          return false;
+			        }else {
+			           	$(this).next(".num").text(num-1);
+			           	$.ajax({
+			           		url:'http://dbshop.com/index.php/Api/cart/edit',
+			           		type: 'post',
+			           		dataType: 'json',
+			           		data: {
+			           			id: idNo,
+			           			is_add: 2,
+			           		},
+			           		success: function(res){
+			           			if(res.error_no == 0){
+			           			}else{
+			           				alert(res.msg);
+			           			}
+			           		},
+			           		error: function(){
+			           			alert("网络错误!");
+			           		}
+			           	})
+			        }
+			    });
+			    $(".numJia").click(function(){
+			    	var num = parseInt($(this).prev(".num").text());
+			    	var idNo = $(this).closest(".goodsList").data("id");
+			    	$(this).prev(".num").text(num+1);
+			    	$.ajax({
+		           		url:'http://dbshop.com/index.php/Api/cart/edit',
+		           		type: 'post',
+		           		dataType: 'json',
+		           		data: {
+		           			id: idNo,
+		           			is_add: 1,
+		           		},
+		           		success: function(res){
+		           			if(res.error_no == 0){
+		           			}else{
+		           				alert(res.msg);
+		           			}
+		           		},
+		           		error: function(){
+		           			alert("网络错误!");
+		           		}
+		           	})
+			  	});
+				//勾选
+				$(".checkIconSpan").click(function(){
+					// $(this).toggleClass("checkedIcon");
+					if($(this).hasClass("checkedIcon")){
+						$(this).removeClass("checkedIcon");
+						$(".all").removeClass("checkedIcon");
+					}else{
+						$(this).addClass("checkedIcon");
+					}
+				});
+				//全选
+				$(".all").click(function(){
+					// $(this).toggleClass("checkedIcon");
+					// $(".checkIcon").toggleClass("checkedIcon");if($(this).hasClass("checkedIcon")){
+					if($(this).hasClass("checkedIcon")){
+						$(this).removeClass("checkedIcon");
+						$(".checkIconSpan").removeClass("checkedIcon");
+						$(".all").removeClass("checkedIcon");
+					}else{
+						$(this).addClass("checkedIcon");
+						$(".checkIconSpan").addClass("checkedIcon");
+						$(".all").addClass("checkedIcon");
+					}
+				});
+				//删除
+				$(".deleteGoods").click(function(){
+					var idNo = $(this).closest(".goodsList").data("id");
+					$.ajax({
+						url: 'http://dbshop.com/index.php/Api/cart/del',
+						type: 'post',
+						dataType: 'json',
+						data: {
+							id: idNo,
+						},
+						success: function(res){
+							if (res.error_no == 0) {
+								alert("删除成功!");
+								$.ajax({
+									url: "http://dbshop.com/index.php/Api/cart/cartlist",
+									type: "post",
+									dataType: "json",
+									data: {
+										token: userId,
+									},
+									success: function(res){
+										if (res.error_no == 0) {
+											var goodsStr = "";
+											var g = res.data.cart;
+											for(var i=0; i<g.length; i++){
+												goodsStr += "<div class='goodsList' data-id='"+g[i].id+"'><span class='checkIcon checkIconSpan'></span><div class='goodsInfo clearfix'><img src='"+g[i].image+"'><div class='info'><h6>"+g[i].goods_name+"</h6><p>玫瑰乌龙+玫瑰红茶</p><div class='numChange'><span class='numJian'>-</span><span class='num'>"+g[i].goods_num+"</span><span class='numJia'>+</span></div></div><div class='del'><h6 class='deleteGoods'>删除</h6><span>￥"+g[i].goods_price/100+".00</span></div></div></div>";
+											}
+											$(".goodsLists").html(goodsStr);
+										}else{
+											alert(res.msg);
+										}
+									},
+									error: function(){
+										alert("网络错误!");
+									}
+								})
+							}else{
+								alert(res.msg);
+							}
+						},
+						error:function(){
+							alert("网络错误！");
+						}
+					})
+				});
 			}else{
-				alert(res.msg)
+				alert(res.msg);
 			}
 		},
 		error: function(){
@@ -49,31 +169,49 @@ $(function(){
 			var goodsStr = "";
 			var g = res.data.cart;
 			for(var i=0; i<g.length; i++){
-				goodsStr += "<div class='goodsList'><span class='checkIcon'></span><div class='goodsInfo clearfix'><img src='"+g[i].image+"'><div class='info'><h6>"+g[i].goods_name+"</h6><p>玫瑰乌龙+玫瑰红茶</p><div class='numChange'><span class='numJian'>-</span><span class='num'>"+g[i].goods_num+"</span><span class='numJia'>+</span></div></div><div class='del'><h6>删除</h6><span>￥"+g[i].goods_price/100+"</span></div></div></div>"
+				goodsStr += "<div class='goodsList' data-id='"+g[i].id+"'><span class='checkIcon checkIconSpan'></span><div class='goodsInfo clearfix'><img src='"+g[i].image+"'><div class='info'><h6>"+g[i].goods_name+"</h6><p>玫瑰乌龙+玫瑰红茶</p><div class='numChange'><span class='numJian'>-</span><span class='num'>"+g[i].goods_num+"</span><span class='numJia'>+</span></div></div><div class='del'><h6 class='deleteGoods'>删除</h6><span>￥"+g[i].goods_price/100+".00</span></div></div></div>";
 			}
 			$(".goodsLists").html(goodsStr);
+			//数量的增减
+		 	$(".numJian").click(function(){
+		        var num = parseInt($(this).next(".num").text());
+		        var idNo = $(this).closest(".goodsList").data("id");
+		        if(num == 1){
+		          return false;
+		        }else {
+		           	$(this).next(".num").text(num-1);
+		        }
+		    });
+		    $(".numJia").click(function(){
+		    	var num = parseInt($(this).prev(".num").text());
+		    	var idNo = $(this).closest(".goodsList").data("id");
+		    	$(this).prev(".num").text(num+1);
+		  	});
+			//勾选
+			$(".checkIconSpan").click(function(){
+				// $(this).toggleClass("checkedIcon");
+				if($(this).hasClass("checkedIcon")){
+					$(this).removeClass("checkedIcon");
+					$(".all").removeClass("checkedIcon");
+				}else{
+					$(this).addClass("checkedIcon");
+				}
+			});
+			//全选
+			$(".all").click(function(){
+				// $(this).toggleClass("checkedIcon");
+				// $(".checkIcon").toggleClass("checkedIcon");if($(this).hasClass("checkedIcon")){
+				if($(this).hasClass("checkedIcon")){
+					$(this).removeClass("checkedIcon");
+					$(".checkIconSpan").removeClass("checkedIcon");
+					$(".all").removeClass("checkedIcon");
+				}else{
+					$(this).addClass("checkedIcon");
+					$(".checkIconSpan").addClass("checkedIcon");
+					$(".all").addClass("checkedIcon");
+				}
+			});
 		}
 	})
-	//勾选
-	$(".checkIcon").click(function(){
-		$(this).toggleClass("checkedIcon");
-	})
-	//全选
-	$(".all").click(function(){
-		$(this).toggleClass("checkedIcon");
-		$(".checkIcon").toggleClass("checkedIcon");
-	})
-	//数量的增减
- 	$(".numJian").click(function(){
-        var num = parseInt($(this).next(".num").text())
-        if(num == 1){
-          return false;
-        }else {
-           	$(this).next(".num").text(num-1)
-        }
-    })
-    $(".numJia").click(function(){
-    	var num = parseInt($(this).prev(".num").text())
-    	$(this).prev(".num").text(num+1)
-  	})
+	
 })
